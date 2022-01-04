18119a14fc1fc2baa850706559830e94fda1d306
Srimany123 <58987560+Srimany123@users.noreply.github.com>
2022-01-04 11:18:01 +0530

    Create SimplePy.py

---
+++ SimplePy.py
@@ -0,0 +1,6 @@
+print("hello! this is the program to run simple python from git")
+a=int(input("enter a number between 1 to 6 to guess random number : ")
+import random
+r=random.randint(1,6)
+if r==a:
+  print("you guessed it right")
